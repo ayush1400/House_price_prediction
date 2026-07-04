5b61c874c376171e39c68a90c78445af58a25929
ayush1400 <ayushyadavv1400@gmail.com>
2026-07-04 17:43:19 +0530

    update route

--- app.py
+++ app.py
@@ -1,29 +1,30 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 import pickle
 import numpy as np
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
-model = pickle.load(
-    open("house_price_model.pkl", "rb")
-)
 
-@app.route('/predict', methods=['POST'])
+model = pickle.load(open("house_price_model.pkl", "rb"))
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/predict", methods=["POST"])
 def predict():
-
     data = request.json
 
     features = np.array(
-        data['features']
+        data["features"]
     ).reshape(1, -1)
 
     prediction = model.predict(features)
 
     return jsonify({
-        "predicted_price":
-        float(prediction[0])
+        "predicted_price": float(prediction[0])
     })
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000)
